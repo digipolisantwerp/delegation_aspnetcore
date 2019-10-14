4e2272f9d06db775ddca47ba0fb68147af484361
ICA\ex02427 <Rob.Liekens@digipolis.be>
2019-10-14 16:36:41 +0200

    fixed memory leaks

--- src/Digipolis.Delegation/Jwt/JwtSigningKeyResolver.cs
+++ src/Digipolis.Delegation/Jwt/JwtSigningKeyResolver.cs
@@ -22,19 +22,15 @@ namespace Digipolis.Delegation.Jwt
         private readonly HttpClient _client;
         private readonly bool _cachingEnabled;
         private const string CACHE_KEY = "JwtSigningKey";
-        private readonly ILogger<JwtSigningKeyResolver> _logger;
 
-        public JwtSigningKeyResolver(IMemoryCache cache, IOptions<DelegationOptions> options, HttpMessageHandler handler, ILogger<JwtSigningKeyResolver> logger)
+        public JwtSigningKeyResolver(IMemoryCache cache, IOptions<DelegationOptions> options, HttpMessageHandler handler)
         {
-            if (cache == null) throw new ArgumentNullException(nameof(cache), $"{nameof(cache)} cannot be null");
             if (options == null || options.Value == null) throw new ArgumentNullException(nameof(options), $"{nameof(options)} cannot be null");
             if (handler == null) throw new ArgumentNullException(nameof(handler), $"{nameof(handler)} cannot be null");
-            if (logger == null) throw new ArgumentNullException(nameof(logger), $"{nameof(logger)} cannot be null");
 
-            _cache = cache;
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache), $"{nameof(cache)} cannot be null");
             _options = options.Value;
             _client = new HttpClient(handler, true);
-            _logger = logger;
 
             if (_options.JwtSigningKeyCacheDuration > 0)
             {
@@ -58,8 +54,7 @@ namespace Digipolis.Delegation.Jwt
 
         private SecurityKey GetSecurityKeyFromX5u(string x5uUrl, bool allowCached)
         {
-            SecurityKey key = null;
-
+            SecurityKey key;
             if (allowCached && _cachingEnabled)
             {
                 key = _cache.Get(CACHE_KEY) as SecurityKey;
@@ -90,11 +85,11 @@ namespace Digipolis.Delegation.Jwt
         {
             cert = cert.Replace("-----BEGIN PUBLIC KEY-----", "").Replace("-----END PUBLIC KEY-----", "");
 
-            var rsa = DecodeX509PublicKey(Convert.FromBase64String(cert));
-
-            var rsaSecurityKey = new RsaSecurityKey(rsa.ExportParameters(false));
-
-            return rsaSecurityKey;
+            using (var rsa = DecodeX509PublicKey(Convert.FromBase64String(cert)))
+            {
+                var rsaSecurityKey = new RsaSecurityKey(rsa.ExportParameters(false));
+                return rsaSecurityKey;
+            }
         }
 
         private static bool CompareBytearrays(byte[] a, byte[] b)
@@ -117,91 +112,95 @@ namespace Digipolis.Delegation.Jwt
             byte[] SeqOID = { 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00 };
             byte[] seq = new byte[15];
             // ---------  Set up stream to read the asn.1 encoded SubjectPublicKeyInfo blob  ------
-            MemoryStream mem = new MemoryStream(x509key);
-            BinaryReader binr = new BinaryReader(mem);    //wrap Memory Stream with BinaryReader for easy reading
-            byte bt = 0;
-            ushort twobytes = 0;
-
-            try
+            using (MemoryStream mem = new MemoryStream(x509key))
             {
+                BinaryReader binr = new BinaryReader(mem);    //wrap Memory Stream with BinaryReader for easy reading
+                byte bt = 0;
+                ushort twobytes = 0;
 
-                twobytes = binr.ReadUInt16();
-                if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
-                    binr.ReadByte();    //advance 1 byte
-                else if (twobytes == 0x8230)
-                    binr.ReadInt16();   //advance 2 bytes
-                else
-                    return null;
-
-                seq = binr.ReadBytes(15);       //read the Sequence OID
-                if (!CompareBytearrays(seq, SeqOID))    //make sure Sequence for OID is correct
-                    return null;
-
-                twobytes = binr.ReadUInt16();
-                if (twobytes == 0x8103) //data read as little endian order (actual data order for Bit String is 03 81)
-                    binr.ReadByte();    //advance 1 byte
-                else if (twobytes == 0x8203)
-                    binr.ReadInt16();   //advance 2 bytes
-                else
-                    return null;
-
-                bt = binr.ReadByte();
-                if (bt != 0x00)     //expect null byte next
-                    return null;
-
-                twobytes = binr.ReadUInt16();
-                if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
-                    binr.ReadByte();    //advance 1 byte
-                else if (twobytes == 0x8230)
-                    binr.ReadInt16();   //advance 2 bytes
-                else
-                    return null;
-
-                twobytes = binr.ReadUInt16();
-                byte lowbyte = 0x00;
-                byte highbyte = 0x00;
-
-                if (twobytes == 0x8102) //data read as little endian order (actual data order for Integer is 02 81)
-                    lowbyte = binr.ReadByte();  // read next bytes which is bytes in modulus
-                else if (twobytes == 0x8202)
+                try
                 {
-                    highbyte = binr.ReadByte(); //advance 2 bytes
-                    lowbyte = binr.ReadByte();
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8230)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    seq = binr.ReadBytes(15);       //read the Sequence OID
+                    if (!CompareBytearrays(seq, SeqOID))    //make sure Sequence for OID is correct
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8103) //data read as little endian order (actual data order for Bit String is 03 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8203)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    bt = binr.ReadByte();
+                    if (bt != 0x00)     //expect null byte next
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8230)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    byte lowbyte = 0x00;
+                    byte highbyte = 0x00;
+
+                    if (twobytes == 0x8102) //data read as little endian order (actual data order for Integer is 02 81)
+                        lowbyte = binr.ReadByte();  // read next bytes which is bytes in modulus
+                    else if (twobytes == 0x8202)
+                    {
+                        highbyte = binr.ReadByte(); //advance 2 bytes
+                        lowbyte = binr.ReadByte();
+                    }
+                    else
+                        return null;
+                    byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };   //reverse byte order since asn.1 key uses big endian order
+                    int modsize = BitConverter.ToInt32(modint, 0);
+
+                    byte firstbyte = binr.ReadByte();
+                    binr.BaseStream.Seek(-1, SeekOrigin.Current);
+
+                    if (firstbyte == 0x00)
+                    {   //if first byte (highest order) of modulus is zero, don't include it
+                        binr.ReadByte();    //skip this null byte
+                        modsize -= 1;   //reduce modulus buffer size by 1
+                    }
+
+                    byte[] modulus = binr.ReadBytes(modsize);   //read the modulus bytes
+
+                    if (binr.ReadByte() != 0x02)            //expect an Integer for the exponent data
+                        return null;
+                    int expbytes = (int)binr.ReadByte();        // should only need one byte for actual exponent data (for all useful values)
+                    byte[] exponent = binr.ReadBytes(expbytes);
+
+                    // ------- create RSACryptoServiceProvider instance and initialize with public key -----
+                    var rsa = RSA.Create();
+                    RSAParameters RSAKeyInfo = new RSAParameters
+                    {
+                        Modulus = modulus,
+                        Exponent = exponent
+                    };
+                    rsa.ImportParameters(RSAKeyInfo);
+                    return rsa;
                 }
-                else
+                catch (Exception)
+                {
                     return null;
-                byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };   //reverse byte order since asn.1 key uses big endian order
-                int modsize = BitConverter.ToInt32(modint, 0);
-
-                byte firstbyte = binr.ReadByte();
-                binr.BaseStream.Seek(-1, SeekOrigin.Current);
-
-                if (firstbyte == 0x00)
-                {   //if first byte (highest order) of modulus is zero, don't include it
-                    binr.ReadByte();    //skip this null byte
-                    modsize -= 1;   //reduce modulus buffer size by 1
                 }
-
-                byte[] modulus = binr.ReadBytes(modsize);   //read the modulus bytes
-
-                if (binr.ReadByte() != 0x02)            //expect an Integer for the exponent data
-                    return null;
-                int expbytes = (int)binr.ReadByte();        // should only need one byte for actual exponent data (for all useful values)
-                byte[] exponent = binr.ReadBytes(expbytes);
-
-                // ------- create RSACryptoServiceProvider instance and initialize with public key -----
-                var rsa = RSA.Create();
-                RSAParameters RSAKeyInfo = new RSAParameters();
-                RSAKeyInfo.Modulus = modulus;
-                RSAKeyInfo.Exponent = exponent;
-                rsa.ImportParameters(RSAKeyInfo);
-                return rsa;
+                finally { binr.Dispose(); }
             }
-            catch (Exception)
-            {
-                return null;
-            }
-            finally { binr.Dispose(); }
         }
     }
 }
